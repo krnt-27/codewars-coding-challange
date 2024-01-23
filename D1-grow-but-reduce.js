7d09d91fe94789a71a2f28a71a5e4b167418ad64
kurnianto27 <furqonkurnianto27@gmail.com>
2024-01-23 08:53:32 +0700

    challange D1

---
+++ D1-grow-but-reduce.js
@@ -0,0 +1,17 @@
+// Solusi Pertama
+
+// function grow(x) {
+//   let result = x[0];
+//   for (let i = 1; i < x.length; i++) {
+//     result *= x[i];
+//   }
+//   return result;
+// }
+
+// Solusi Kedua
+function grow(x) {
+  const result = x.reduce((acc, curr) => acc * curr, 1);
+  return result;
+}
+console.log(grow([1, 2, 3, 4]));
+console.log(grow([2, 2, 2, 2, 2, 2]));
